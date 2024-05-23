aaf95e4b695d0b6d19155c22f5e69c48e1209805
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-05-23 16:31:27 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(list_1):
+    lista2 = []
+    index=0
+    for element in list_1:
+        if element != "":
+            lista2.append(f"{index}. {element}")
+            index += 1
+    return lista2
 
+   
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(list_2):
+    lista1 = []
+    index = 0
+    for element in list_2:
+        if element != "":
+            lista1.append(f"{index}. {element[::-1]}")
+            index += 1
+    return lista1
